e307721d85f963eb012ec74c47e2e0021ccca2b7
kmk_Play <lkmkl5844@naver.com>
2025-06-17 12:30:16 +0900

    디렉터 기반

---
+++ Train/Assets/_Script/Director/DictionaryDirector.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DictionaryDirector : MonoBehaviour
+{
+    public Game_DataTable Data_Game;
+    public SA_Monster SA_Monster_;
+
+    [Header("몬스터 도감")]
+    [Space(20)]
+    public GameObject 
+
+    private void Start()
+    {
+        
+    }
+}
